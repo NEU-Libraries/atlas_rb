7271e46f01f7a70e878409a4a9a6085c97d90cf7
David Cliff <david.g.cliff@gmail.com>
2023-07-13 19:01:23 +0000

    Bundle gem init

---
+++ spec/atlas/rb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Atlas::Rb do
+  it "has a version number" do
+    expect(Atlas::Rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
