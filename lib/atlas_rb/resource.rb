1bda181e75a329ea8a03080c2e168ef4a3a69019
David Cliff <david.g.cliff@gmail.com>
2023-07-20 17:59:10 +0000

    Stubbing out resource CRUD template

---
+++ lib/atlas_rb/resource.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module AtlasRb
+  class Resource
+    def self.find(id)
+    end
+
+    def self.create
+    end
+
+    def self.destroy(id)
+    end
+
+    def self.update(id) #figure out params
+    end
+  end
+end
