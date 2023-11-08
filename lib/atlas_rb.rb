b125b155ceace28588f896b9b865142e66f08a71
David Cliff <david.g.cliff@gmail.com>
2023-11-08 18:55:05 +0000

    Back to require_relative

--- lib/atlas_rb.rb
+++ lib/atlas_rb.rb
@@ -2,11 +2,14 @@
 
 require "faraday"
 require "faraday/multipart"
-# require_relative "atlas_rb/version"
-# require_relative "atlas_rb/faraday_helper"
-# require_relative "atlas_rb/resource"
-#require_relative "atlas_rb/community"
-Dir['./**/*.rb'].sort_by {|path| path.split("/").length }.each{ |f| require f }
+require_relative "atlas_rb/version"
+require_relative "atlas_rb/faraday_helper"
+require_relative "atlas_rb/resource"
+require_relative "atlas_rb/community"
+require_relative "atlas_rb/collection"
+require_relative "atlas_rb/work"
+require_relative "atlas_rb/file_set"
+require_relative "atlas_rb/file"
 
 module AtlasRb
   class Error < StandardError; end
