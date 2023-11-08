9cacce3c43e12e0a2cc9f8c27248cb5457a6456d
David Cliff <david.g.cliff@gmail.com>
2023-11-08 17:50:35 +0000

    Active gem

--- lib/atlas_rb.rb
+++ lib/atlas_rb.rb
@@ -2,10 +2,7 @@
 
 require "faraday"
 require "faraday/multipart"
-# require_relative "atlas_rb/version"
-# require_relative "atlas_rb/faraday_helper"
-# require_relative "atlas_rb/resource"
-#require_relative "atlas_rb/community"
+require 'require_all'
 require_all "atlas_rb"
 
 module AtlasRb
