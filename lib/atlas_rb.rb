9987e96784baf6662a2673210d90158bab04af28
David Cliff <david.g.cliff@gmail.com>
2023-11-08 17:18:26 +0000

    Add require all gem

--- lib/atlas_rb.rb
+++ lib/atlas_rb.rb
@@ -2,10 +2,11 @@
 
 require "faraday"
 require "faraday/multipart"
-require_relative "atlas_rb/version"
-require_relative "atlas_rb/faraday_helper"
-require_relative "atlas_rb/resource"
-require_relative "atlas_rb/community"
+# require_relative "atlas_rb/version"
+# require_relative "atlas_rb/faraday_helper"
+# require_relative "atlas_rb/resource"
+#require_relative "atlas_rb/community"
+require_all "atlas_rb"
 
 module AtlasRb
   class Error < StandardError; end
