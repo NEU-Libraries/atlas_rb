041af1d73c3a13ce7eb4ea421ad237f3fd09694e
David Cliff <david.g.cliff@gmail.com>
2023-11-09 18:55:35 +0000

    Add JSON parsing

--- lib/atlas_rb/collection.rb
+++ lib/atlas_rb/collection.rb
@@ -5,12 +5,12 @@ module AtlasRb
     ROUTE = "/collections/"
 
     def self.find(id)
-      connection({}).get(ROUTE + id)&.body
+      JSON.parse(connection({}).get(ROUTE + id)&.body)
     end
 
     def self.create(id)
       # params[:parent_id]
-      connection({ parent_id: id }).post(ROUTE)&.body
+      JSON.parse(connection({ parent_id: id }).post(ROUTE)&.body)
     end
 
     def self.destroy(id)
@@ -18,14 +18,14 @@ module AtlasRb
     end
 
     def self.children(id)
-      connection({}).get(ROUTE + id + '/children')&.body
+      JSON.parse(connection({}).get(ROUTE + id + '/children')&.body)
     end
 
     def self.update(id, xml_path)
       payload = { binary: Faraday::Multipart::FilePart.new(File.open(xml_path),
                                                            "application/xml",
                                                            File.basename(xml_path)) }
-      multipart({}).patch(ROUTE + id, payload)&.body
+      JSON.parse(multipart({}).patch(ROUTE + id, payload)&.body)
     end
   end
 end
--- lib/atlas_rb/community.rb
+++ lib/atlas_rb/community.rb
@@ -5,11 +5,11 @@ module AtlasRb
     ROUTE = "/communities/"
 
     def self.find(id)
-      connection({}).get(ROUTE + id)&.body
+      JSON.parse(connection({}).get(ROUTE + id)&.body)
     end
 
     def self.create(id = nil)
-      connection({ parent_id: id }).post(ROUTE)&.body
+      JSON.parse(connection({ parent_id: id }).post(ROUTE)&.body)
     end
 
     def self.destroy(id)
@@ -17,14 +17,14 @@ module AtlasRb
     end
 
     def self.children(id)
-      connection({}).get(ROUTE + id + '/children')&.body
+      JSON.parse(connection({}).get(ROUTE + id + '/children')&.body)
     end
 
     def self.update(id, xml_path)
       payload = { binary: Faraday::Multipart::FilePart.new(File.open(xml_path),
                                                            "application/xml",
                                                            File.basename(xml_path)) }
-      multipart({}).patch(ROUTE + id, payload)&.body
+      JSON.parse(multipart({}).patch(ROUTE + id, payload)&.body)
     end
   end
 end
