e1f82d215ad64e7f85007391c1149d460aff6f98
David Cliff <david.g.cliff@gmail.com>
2024-01-03 15:36:41 +0000

    JSON parsing of Work model

--- lib/atlas_rb/work.rb
+++ lib/atlas_rb/work.rb
@@ -5,12 +5,12 @@ module AtlasRb
     ROUTE = "/works/"
 
     def self.find(id)
-      connection({}).get(ROUTE + id)&.body
+      JSON.parse(connection({}).get(ROUTE + id)&.body)
     end
 
     def self.create(id)
       # params[:collection_id]
-      connection({ collection_id: id }).post(ROUTE)&.body
+      JSON.parse(connection({ collection_id: id }).post(ROUTE)&.body)
     end
 
     def self.destroy(id)
@@ -21,7 +21,7 @@ module AtlasRb
       payload = { binary: Faraday::Multipart::FilePart.new(File.open(xml_path),
                                                            "application/xml",
                                                            File.basename(xml_path)) }
-      multipart({}).patch(ROUTE + id, payload)&.body
+      JSON.parse(multipart({}).patch(ROUTE + id, payload)&.body)
     end
   end
 end
