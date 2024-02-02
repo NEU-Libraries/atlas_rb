a782a3703b2119629ecd3491e9b3f5eef0c3a8df
David Cliff <david.g.cliff@gmail.com>
2024-02-02 16:52:36 +0000

    Prototyping method to use Atlas preview route

--- lib/atlas_rb/resource.rb
+++ lib/atlas_rb/resource.rb
@@ -9,5 +9,12 @@ module AtlasRb
       { "klass" => result.first[0].capitalize,
         "resource" => result.first[1] }
     end
+
+    def self.preview(xml_path)
+      payload = { binary: Faraday::Multipart::FilePart.new(File.open(xml_path),
+                                                           "application/xml",
+                                                           File.basename(xml_path)) }
+      multipart({ work_id: id }).post('/resources/preview', payload)&.body
+    end
   end
 end
