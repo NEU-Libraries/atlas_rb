015004b4e5cd6df6f960e65975666038318ada8d
David Cliff <david.g.cliff@gmail.com>
2024-08-27 13:55:54 +0000

    Wrong placement of end fix

--- lib/atlas_rb/blob.rb
+++ lib/atlas_rb/blob.rb
@@ -3,28 +3,28 @@
 module AtlasRb
   class Blob < Resource
     ROUTE = "/files/"
-  end
 
-  def self.find(id)
-    connection({}).get(ROUTE + id)&.body
-  end
+    def self.find(id)
+      connection({}).get(ROUTE + id)&.body
+    end
 
-  def self.create(id, blob_path)
-    payload = { binary: Faraday::Multipart::FilePart.new(File.open(blob_path),
-                                                         "application/octet-stream",
-                                                         File.basename(blob_path)) }
+    def self.create(id, blob_path)
+      payload = { binary: Faraday::Multipart::FilePart.new(File.open(blob_path),
+                                                          "application/octet-stream",
+                                                          File.basename(blob_path)) }
 
-    multipart({ work_id: id }).post(ROUTE, payload)&.body
-  end
+      multipart({ work_id: id }).post(ROUTE, payload)&.body
+    end
 
-  def self.destroy(id)
-    connection({}).delete(ROUTE + id)
-  end
+    def self.destroy(id)
+      connection({}).delete(ROUTE + id)
+    end
 
-  def self.update(id, blob_path)
-    payload = { binary: Faraday::Multipart::FilePart.new(File.open(blob_path),
-                                                         "application/octet-stream",
-                                                         File.basename(blob_path)) }
-    multipart({}).patch(ROUTE + id, payload)&.body
+    def self.update(id, blob_path)
+      payload = { binary: Faraday::Multipart::FilePart.new(File.open(blob_path),
+                                                          "application/octet-stream",
+                                                          File.basename(blob_path)) }
+      multipart({}).patch(ROUTE + id, payload)&.body
+    end
   end
 end
