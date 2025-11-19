e9d4631d26a307e9cf7583bfea349e88f4169b15
David Cliff <dgcliff@northeastern.edu>
2025-11-19 19:55:51 +0000

    First pass at fully integrated system token via header and nuid via param

--- lib/atlas_rb/authentication.rb
+++ lib/atlas_rb/authentication.rb
@@ -5,12 +5,17 @@ module AtlasRb
     extend AtlasRb::FaradayHelper
 
     def self.login(nuid)
-      JSON.parse(connection({ nuid: nuid }).post('/token')&.body)["token"]
+      # JSON.parse(connection({ nuid: nuid }).post('/token')&.body)["token"]
+      # need hash - id, name, token => ...
+      JSON.parse(connection({}, nuid).get('/user')&.body)
     end
 
     def self.groups(nuid)
-      token = login(nuid)
-      # TODO
+      # user_details = login(nuid)
+      # token = user_details[:token] ...
+      # TODO - need to update atlas login to give back name, id, and token upon logging in
+      # result = JSON.parse(connection({ token: token }).post('/users/2/groups')&.body)["user"]["groups"]
+      JSON.parse(connection({}, nuid).get('/user')&.body)["groups"]
     end
   end
 end
--- lib/atlas_rb/faraday_helper.rb
+++ lib/atlas_rb/faraday_helper.rb
@@ -2,19 +2,29 @@
 
 module AtlasRb
   module FaradayHelper
-    def connection(params)
+    def connection(params, nuid=nil)
       Faraday.new(
         url: ENV.fetch("ATLAS_URL", nil),
         params: params,
-        headers: { "Content-Type" => "application/json" }
+        headers: {
+          "Content-Type" => "application/json",
+          "Authorization: Bearer vK6LgnPNq2tUWy8UrPwU",
+          "User: NUID #{nuid}"
+        }
       ) do |f|
         f.response :follow_redirects
         f.adapter Faraday.default_adapter
       end
     end
 
-    def multipart(_params)
-      Faraday.new(url: ENV.fetch("ATLAS_URL", nil)) do |f|
+    def multipart(_params, nuid=nil)
+      Faraday.new(
+        url: ENV.fetch("ATLAS_URL", nil),
+        headers: {
+          "Authorization: Bearer vK6LgnPNq2tUWy8UrPwU",
+          "User: NUID #{nuid}"
+        }
+      ) do |f|
         f.request :multipart
         f.request :url_encoded
       end
